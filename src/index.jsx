aa89e75944b2a733f8de53f837b57dad480bfbe5
harkemaj <jmware21@aol.com>
2024-09-19 10:19:02 -0500

    First try at updating JS to make the form work

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-function showAnswer(response) {
-  alert(response.data.answer);
-}
+function generatePoem(event) {
+  event.preventDefault(); let instructionsInput=document.querySelector("#search-form-input");
+
 
 let apiKey = `a746fat433eb052605030e2eod760f33`;
 let context =
   "be creative and provide a elquent poem in French based on the word(s) or phrase(s) entered";
-let prompt = "Flower";
+let prompt = ${instructionsInput.value};
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 console.log("Processing");
-axios.get(apiUrl).then(showAnswer);
+axios.get(apiUrl).then(generatePoem);}
+let poemFormElement = document.querySelector("#search-form-input");
+poemFormElement.addEventListener("submit", generatePoem);
