9da807814776293ca78bc930b1a98f677a31db48
harkemaj <jmware21@aol.com>
2024-09-19 10:40:50 -0500

    Update JS, but the system can't read the value in the "prompt" line. Researching how to fix.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,30 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
-  event.preventDefault(); let instructionsInput=document.querySelector("#search-form-input");
+  event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
 
-let apiKey = `a746fat433eb052605030e2eod760f33`;
-let context =
-  "be creative and provide a elquent poem in French based on the word(s) or phrase(s) entered";
-let prompt = ${instructionsInput.value};
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-console.log("Processing");
-axios.get(apiUrl).then(generatePoem);}
-let poemFormElement = document.querySelector("#search-form-input");
+  let apiKey = `a746fat433eb052605030e2eod760f33`;
+  let context =
+    "Be creative and provide a elquent poem in French based on the word(s) or phrase(s) entered";
+
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
